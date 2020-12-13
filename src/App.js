967f9cd07db9effa75dcb0b6ba2b1272abd2bb9b
Zaourbek Gerikhanov <zaur.ataginsky@gmail.com>
2020-12-13 18:15:17 +0100

    core: Import Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
 let name = "Worker";
-let version = '0.1.0-1';
+let version = '0.1.0 - build ' + new Date().toUTCString();
 
 function App() {
 
-	return (<div>
-			<h1>{name}</h1>
-			<h5>Version: {version}</h5>
-		</div>
+	return (
+		<Router>
+			<div>
+				<h1>{name}</h1>
+				<h5>Version: {version}</h5>
+				<ul>
+					<li><Link to="/">Home</Link></li>
+					<li><Link to="/about">About</Link></li>
+				</ul>
+			</div>
+
+			<Switch>
+				<Route path="/about" component={About} />
+				<Route path="/" component={Home} />
+			</Switch>
+		</Router>
 	);
 
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,3 @@
+export default function Home() {
+	return (<h>Welcome to Home</h>);
+}

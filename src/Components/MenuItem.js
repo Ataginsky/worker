e763c5d08fa8d9f631b32c13ce9ceb2f903a922c
Zaourbek Gerikhanov <zaur.ataginsky@gmail.com>
2020-12-26 11:04:58 +0100

    feat: MenuItem & RadioInput visual update

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 
 export default function MenuItem(props) {
+
+	let className = "px-4 py-1 text-gray-500"
+	className += " border-b-3 border-transparent hover:border-yellow-400";
+	// let activeClass = props.active ? 'text-gray-700 rounded bg-yellow-200' : '';
+	let activeClass = props.active ? 'text-gray-900  border-yellow-400' : '';
+	
 	return (
-		<span className="px-4"><Link to={props.to}>{props.text}</Link></span>
+		<span className={className + ' ' + activeClass}><Link to={props.to}>{props.text}</Link></span>
 	);
 }
--- src/Components/RadioInput.js
+++ src/Components/RadioInput.js
@@ -1,11 +1,23 @@
 export default function RadioItem(props) {
-	let className = 'cursor-pointer inline-block m-1 px-3 py-2 bg-white border-gray-200 border-2';
+	/* let className = 'cursor-pointer inline-block m-1 px-3 py-2 bg-white border-gray-200 border-2';
 	className += ' hover:border-yellow-500';
 	className += props.checked === props.value ? ' bg-yellow-50 border-yellow-500' : '';
+ */
+
+	/* let className = 'cursor-pointer inline-block m-1 px-3 py-2 bg-white transition-shadow rounded';
+	className += ' hover:border-yellow-500 hover:shadow-xl';
+	className += props.checked === props.value ? ' bg-yellow-50 border-yellow-500 shadow-lg' : ' shadow-sm';
+ */
+
+	// let bgColor = 'hsl(43 77% 78% / 1)';
+	let className = 'cursor-pointer inline-block m-1 px-3 py-2 rounded border border-yellow-300';
+	className += ' transition-all hover:bg-white hover:shadow';
+	className += props.checked === props.value ? ' bg-white border-yellow-300' : ' bg-gray-100';
+
 
 	return (
 		<label className={className}>
-			<input 	className="mr-2" type="radio" 
+			<input 	className="mr-2 " type="radio" 
 							radioGroup={props.radioGroup} 
 							name={props.name} 
 							checked={props.checked === props.value} 

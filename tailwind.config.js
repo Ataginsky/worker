77510ed7249913b0998ffc4ecda4c2c10af20cf0
Zaourbek Gerikhanov <zaur.ataginsky@gmail.com>
2020-12-26 11:01:41 +0100

    core: Add Tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
-// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
 		fontFamily: {
 			'body': ['Inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'],
+		},
+		colors: {
+			// Build your palette here
+      transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+      white: colors.white,
+			orange: colors.orange,
+			gray: colors.trueGray,
+      red: colors.rose,
+			blue: colors.lightBlue,
+			green: colors.lime,
+			yellow: colors.amber,
+		},
+		borderWidth: {
+			'0': '0',
+			DEFAULT: '1px',
+			'2': '2px',
+			'3': '3px',
+			'4': '4px',
+			'6': '6px',
+			'8': '8px',
 		},
     extend: {},
   },

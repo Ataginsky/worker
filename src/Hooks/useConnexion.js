031132246db87ee2d2d3b6b77cd4e9b419acfb99
Zaourbek Gerikhanov <zaur.ataginsky@gmail.com>
2021-03-15 22:00:11 +0100

    feat: Update useConnection hook to window.navigator

--- src/Hooks/useConnexion.js
+++ src/Hooks/useConnexion.js
@@ -8,17 +8,37 @@ export default function useConnexion() {
 	useEffect(() => {
 		let connectionState = true;
 
+		if(! window.g_software_app?.useConnexion?.count) {
+			window.g_software_app = {
+				useConnexion: {
+					count: 0,
+					timerId: undefined,
+				}
+			}
+
+			window.g_software_app.useConnexion.count = 1;
+		}
+		else
+			window.g_software_app.useConnexion.count++;
+		
+
 		const intervalId = setInterval(() => {
 
-			if (connectionState !== window.navigator.$$internetAvaible) {
+			if(connectionState !== window.navigator.onLine) {
+				connectionState = window.navigator.onLine;
+				setCon(window.navigator.onLine);
+			}
+
+			/* if (connectionState !== window.navigator.$$internetAvaible) {
 				connectionState = window.navigator.$$internetAvaible;
 				setCon(window.navigator.$$internetAvaible);
-			}
+			} */
 
 		}, 1000);
 
 		return () => {
 			clearInterval(intervalId);
+			window.g_software_app.useConnexion.count--;
 		}
 	}, []);
 
